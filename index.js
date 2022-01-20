013630c21204d8cf23d9ae406fc983c71a4b93b9
ElyBeske <ely.beske@yahoo.com>
2022-01-19 23:38:14 -0500

    First 2 paths

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your Code Here
+let answer1 = window.prompt ('You goin left or right?')
+if(answer1 === 'left'){
+    let answer2 = window.prompt ('The path leads you upwards. Eventually you find yourself in a dimly lit room with two large, wooden doors. Which door will you try?')
+}
+else if(answer1 === 'right'){
+    let answer2 = window.prompt ('The passage on the right heads deep into the mountain at a steady decline, the air is damp and musty. After what seems like hours, the tunnel opens up to a large cavern. A narrow path through leads along the wall, at the end appears to be a torch lit door. Across the abyss you see another tunnel, the gap seems small enough you think you can jump over.')
+}
